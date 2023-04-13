d600f073c4fc290c695cdcc6a62a0dd22b8cda80
econexpert <econexpert@users.noreply.github.com>
2023-04-13 10:37:40 +0300

    Update public-syncroencryptionGPG.py

--- public-syncroencryptionGPG.py
+++ public-syncroencryptionGPG.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Listbox, Toplevel, ttk
 from tkinter.messagebox import showinfo
 from tkinter import filedialog
 import os
@@ -22,6 +22,179 @@ except:
             f.close()
         import settings
 
+class FTPClient:
+    def __init__(self, server, username, password):
+        self.server = server
+        self.username = username
+        self.password = password
+        self.ftp_error = False
+        try:
+            self.ftp = ftplib.FTP(self.server, self.username, self.password)
+        except ftplib.all_errors as e:
+            print("some issue" , e)
+            tk.messagebox.showerror("FTP error", f"Error connecting to {settings.FTP}. Reason: \n {e}")
+            self.ftp_error = True
+            return
+        print(self.ftp.getwelcome())
+        self.ftp.cwd('/')
+        self.directory_contents = []
+
+    def get_directory_contents(self, directory):
+        self.directory_contents = []
+        self.ftp.cwd(directory)
+        self.ftp.retrlines('LIST', self.append_to_directory_contents)
+        return self.directory_contents
+
+    def append_to_directory_contents(self, line):
+        self.directory_contents.append(line)
+
+    def create_new_directory(self, dirname):
+        self.ftp.mkd(dirname)
+    
+    def upload_file_ftp(self, file_path, ftp_path):
+        try:
+            if file_path == "":
+                print("no file selected")
+                return
+            print("selected: ", file_path)
+        except:
+            print("no file selected")
+            return   # exit if no file selected
+        self.file_name = os.path.basename(file_path)
+        self.local_file_time = os.path.getmtime(file_path)
+        self.local_file_time = datetime.datetime.utcfromtimestamp(self.local_file_time).strftime('%Y-%m-%d %H:%M:%S')
+        print("local file time:", self.local_file_time)
+        try:
+            print("FTP upload attempt, server:" , settings.FTP)
+# ----  check if file already exists on FTP
+            try:
+                file_size = self.ftp.size(self.file_name)
+                file_date_string = self.ftp.sendcmd('MDTM ' + self.file_name)[4:]
+                file_date = datetime.datetime.strptime(file_date_string, '%Y%m%d%H%M%S')
+                message = f"The file {self.file_name} exists \n with size {file_size} bytes, \n last modified on {file_date}. \n Your file modifed on {self.local_file_time}. Rewrite file on FTP?"
+                answer = tk.messagebox.askyesno("File conflict on FTP", message)
+                if answer:
+                    print("will replace file on FTP")
+                else:
+                    print("Rename cancelled. The file has not been renamed.")
+                    return
+            except ftplib.error_perm:
+                print(f"The file {file_path} does not exist on the remote server. Proceeeding to upload")
+# -----  continue with data upload
+            file_size = os.path.getsize(file_path)
+            # Upload the file in chunks and track the progress with tqdm
+            chunk_size = 1024 * 1024 # 1 MB
+            uploaded_size = 0
+            with tqdm(total=file_size, unit='B', unit_scale=True, desc='Uploading', miniters=1) as pbar:
+                with open(file_path, 'rb') as f:
+                    while True:
+                        chunk = f.read(chunk_size)
+                        chunk_stream = BytesIO(chunk)
+                        if not chunk:
+                            break
+                        self.ftp.storbinary('STOR ' + self.file_name, chunk_stream)
+                        uploaded_size += len(chunk)
+                        pbar.update(len(chunk))
+#            self.ftp.quit()
+            tk.messagebox.showinfo('Success','File ' + self.file_name + ' uploaded')
+        except ftplib.all_errors as e:
+            print("some issue with FTP",  e)
+            tk.messagebox.showerror("FTP error", f"Some issue with {settings.FTP} server.")
+
+    def download_file_ftp(self, filename, local_path):
+
+        # Get the size of the remote file
+        file_size = self.ftp.size(filename)
+        chunk_size=1024*1024
+    # Download the file in chunks and track the progress with tqdm
+        downloaded_size = 0
+        with tqdm(total=file_size, unit='B', unit_scale=True, desc='Downloading', miniters=1) as pbar:
+            with open(local_path, 'wb') as f:
+                def callback(chunk):
+                    nonlocal downloaded_size
+                    downloaded_size += len(chunk)
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+                self.ftp.retrbinary('RETR ' + filename, callback, chunk_size)    
+        print("downloaded file", local_path) 
+        tk.messagebox.showinfo("Download complete",f"Downloaded {filename} successfully\nin {local_path} ")
+
+
+    def download_file_ftp3(self, filename, local_path):
+        print("attempting download...", filename, local_path)
+        with open(local_path, "wb") as f:   
+            self.ftp.retrbinary("RETR " + filename, f.write)
+            print("downloaded file", local_path) 
+            tk.messagebox.showinfo("Download complete",f"Downloaded {filename} successfully\nin {local_path} ")
+
+class SelectFolder(Toplevel):
+    def __init__(self, master = None):
+        super().__init__(master = master)
+
+        self.file_local = ""
+        print(self.file_local)
+        self.current_directory = "/"
+        self.title("Select folder and upload:")
+        self.geometry("670x300")
+        self.label = tk.Label(self, text="Select folder on FTP:")
+        self.label.pack(side=tk.TOP)
+
+        self.dir_var = tk.StringVar(value='Select a folder')
+        self.dir_listbox = tk.Listbox(self, listvariable=self.dir_var)
+        self.dir_listbox.pack(fill=tk.BOTH, expand=True)
+        self.refresh_button = tk.Button(self, text='Refresh', command=self.populate_listbox)
+        self.refresh_button.pack(side=tk.LEFT)
+        self.new_dir_entry = tk.Entry(self)
+        self.new_dir_entry.pack(side=tk.LEFT)
+
+        self.new_dir_button = tk.Button(self, text='Create New Folder', command=self.create_new_directory)
+        self.new_dir_button.pack(side=tk.LEFT)
+        self.open_dir_button = tk.Button(self, text='Open Folder', command=self.open_directory)
+        self.open_dir_button.pack(side=tk.LEFT)
+        self.upload_button = tk.Button(self, text='Upload file', command=self.upload_file)
+        self.upload_button.pack(side=tk.LEFT)
+        self.exit_button = tk.Button(self, text="Close", command=self.destroywindow)
+        self.exit_button.pack(side=tk.LEFT)
+        self.ftp_client = FTPClient(settings.FTP, settings.USERNAME, settings.PASSWORD)
+        if self.ftp_client.ftp_error == True:
+            print("bad ftp returned. Closing window")
+            self.destroywindow()
+            return
+        self.populate_listbox() # end
+
+    def upload_file(self):
+        self.ftp_client.upload_file_ftp(self.file_local, self.current_directory)
+        self.populate_listbox() # refresh after upload
+
+    def populate_listbox(self):
+        self.dir_var.set('Loading...')
+        self.master.update()
+        directory_contents = self.ftp_client.get_directory_contents("")#'/')
+        self.dir_listbox.delete(0, tk.END)
+        for item in directory_contents:
+            self.dir_listbox.insert(tk.END, item)
+
+    def create_new_directory(self):
+        dirname = self.new_dir_entry.get()
+        self.ftp_client.create_new_directory(dirname)
+        self.new_dir_entry.delete(0, tk.END)
+        self.populate_listbox()
+
+    def open_directory(self):
+        selected_item = self.dir_listbox.get(self.dir_listbox.curselection())
+        selected_item_type = selected_item.split()[0]
+        selected_item_name = selected_item.split()[-1]
+        self.current_directory = selected_item_name # mans
+        if selected_item_type.startswith('d'):
+            directory_contents = self.ftp_client.get_directory_contents(selected_item_name)
+            self.dir_listbox.delete(0, tk.END)
+            for item in directory_contents:
+                self.dir_listbox.insert(tk.END, item)
+    def destroywindow(self):
+        if hasattr(SelectFolder,'ftp'): 
+            print("closing FTP connection. Return from FTP: ", self.ftp.close())
+        self.destroy()
+        
 class App(tk.Tk):
   def __init__(self):
     super().__init__()
@@ -36,7 +209,7 @@ class App(tk.Tk):
 
     # configure the root window
     self.title('Encrypt files')
-    self.geometry('300x400')
+    self.geometry('300x450')
     self.notebook = ttk.Notebook(self)
     self.notebook.pack(pady=10, expand=True)
     self.notebook.option_add('*Dialog.msg.font', 'Arial 11')
@@ -69,17 +242,20 @@ class App(tk.Tk):
     self.recipient_entry = tk.Entry(self.frame1, width=30, show ="*")
     self.recipient_entry.insert(-1,settings.GNURECIPIENT) 
     self.recipient_entry.pack()
-
     self.encrypt_button = tk.Button(self.frame1, text='Encrypt File', command=self.encrypt_file)
     self.encrypt_button.pack()
-    self.upload_button = tk.Button(self.frame1, text='Upload File', command=self.upload_file)
-    self.upload_button.pack()
+
+    self.encrypt_button = tk.Button(self.frame1, text='Select folder and upload', command=self.open_ftp_window)
+    self.encrypt_button.pack()
 
 # ----- frame 2 
     self.connect_button = tk.Button(self.frame2, text="Connect to FTP", command=self.connect)
     self.connect_button.grid(row=0, column=0)
+
     self.file_list = tk.Listbox(self.frame2, width=35, height=6)
-    self.file_list.grid(row=1, column=0) 
+    self.file_list.grid(row=1, columnspan=2) 
+    self.openfolder_button = tk.Button(self.frame2, text="Open folder", command=self.open_ftp_directory)
+    self.openfolder_button.grid(row=5, column=0)
     self.download_button = tk.Button(self.frame2, text="Download", command=self.download)
     self.download_button.grid(row=6, column=0)
     self.file_path_label = tk.Label(self.frame2, text='File Path:')
@@ -123,42 +299,57 @@ class App(tk.Tk):
     self.exit_button = tk.Button(self.frame3, text="Exit", command=self.destroyall)
     self.exit_button.grid(row=6, column = 1)
 
+  def open_ftp_window(self):
+    if hasattr(app,'file_path'): 
+#        SelectFolder()
+        a = SelectFolder()
+        a.file_local = self.file_path
+    else: 
+        print("select a file first")
+
   def destroyall(self):
       app.destroy()
       if hasattr(app,'ftp'): 
           print("closing FTP connection. Return from FTP: ", self.ftp.close())
 
   def connect(self):
-    self.host = settings.FTP
-    self.user = settings.USERNAME
-    self.passwd = settings.PASSWORD
-
-    try:
-        self.ftp = ftplib.FTP(self.host)
-        self.ftp.login(self.user, self.passwd)
-        self.ftp.cwd("/")
-        self.file_list.delete(0, tk.END)
-        self.file_list.insert(tk.END, "Connected to FTP server successfully.")
-        print(self.ftp.getwelcome())
-        self.file_list.insert(tk.END, "Current directory: /")
-        self.populate_file_list()
-    except Exception as e:
-        self.file_list.delete(0, tk.END)
-        self.file_list.insert(tk.END, str(e))
+    self.ftp_client = FTPClient(settings.FTP, settings.USERNAME, settings.PASSWORD)
+    self.populate_file_list()
 
   def populate_file_list(self):
-        files = self.ftp.nlst()
+    self.file_list.insert(tk.END,'Loading...')
+    directory_contents = self.ftp_client.get_directory_contents("")
+    self.file_list.delete(0, tk.END)
+    for item in directory_contents:
+        print(item, item.split()[-1])   # get remote directory contents 
+        self.file_list.insert(tk.END, item.split()[0] + " " +item.split()[-4] + " " +item.split()[-3] + " " +item.split()[-2] + " " +item.split()[-1])
+
+  def open_ftp_directory(self):
+    selected_item = self.file_list.get(self.file_list.curselection())
+    selected_item_type = selected_item.split()[0]
+    selected_item_name = selected_item.split()[-1]
+    print(selected_item_name)
+    self.current_directory = selected_item_name # mans
+    if selected_item_type.startswith('d'):
+        directory_contents = self.ftp_client.get_directory_contents(selected_item_name)
         self.file_list.delete(0, tk.END)
-        self.file_list.insert(tk.END, "Current directory: " + self.ftp.pwd())
-        for f in files:
-            self.file_list.insert(tk.END, f)
+        for item in directory_contents:
+            self.file_list.insert(tk.END, item.split()[0] + " " +item.split()[-1])
 
   def download(self):
       # check if
         if self.file_list.get(tk.ACTIVE) == "":
             print("nothing selected")
             return
-        filename = self.file_list.get(tk.ACTIVE)
+        filename = self.file_list.get(tk.ACTIVE)  
+        fullftppath = filename      
+        fields = filename.split()
+        filename = fields[-1]
+        if filename == ".":
+            print("nothing selected")
+            return
+        print(filename)
+
         local_path = filedialog.askdirectory()
         if local_path:
             local_path = local_path + "/" + filename
@@ -174,12 +365,9 @@ class App(tk.Tk):
             except:
                 print("file not exists on local syteem")
             print("attempting download...")
-            with open(local_path, "wb") as f:   
-                self.ftp.retrbinary("RETR " + filename, f.write)
-                print("downloaded file", local_path) 
-                self.file_path_entry2.delete(0, tk.END)
-                self.file_path_entry2.insert(0, local_path)
-                tk.messagebox.showinfo("Download complete",f"Downloaded {filename} successfully\nin {local_path} ")
+            self.ftp_client.download_file_ftp(filename,local_path)
+            self.file_path_entry2.delete(0, tk.END)
+            self.file_path_entry2.insert(0, local_path)
 
   def save_settings(self):
     # Define the name of the variable you want to change
@@ -213,9 +401,6 @@ class App(tk.Tk):
         return # not show confirmation
     showinfo(title='Information', message='Settings saved')
 
-  def button_clicked(self):
-    showinfo(title='Information', message='Hello!')
-
   def select_file(self):
   # Get current working directory and open file dialog
     current_dir = os.getcwd()
@@ -226,10 +411,10 @@ class App(tk.Tk):
 
   def select_file2(self):  # for second frame
     current_dir = os.getcwd()
-    file_path = filedialog.askopenfilename(initialdir=current_dir, title='Select File')
-    print("selected file path:",file_path)
+    self.file_path = filedialog.askopenfilename(initialdir=current_dir, title='Select File')
+    print("selected file path:",self.file_path)
     self.file_path_entry2.delete(0, tk.END)
-    self.file_path_entry2.insert(0, file_path)
+    self.file_path_entry2.insert(0, self.file_path)
 
   def encrypt_file(self):
     self.file_path = self.file_path_entry.get()
@@ -239,7 +424,7 @@ class App(tk.Tk):
         return
     self.file_name = os.path.basename(self.file_path)
 
-    # Use GPG 
+    # Use GPG, command line for GPG encyption 
     command = ["gpg", "--symmetric", "--armor", "--batch", "--passphrase", self.recipient_entry.get()]
     # Encrypt file using signer's private key
     with open(self.file_path, 'rb') as f:
@@ -286,57 +471,6 @@ class App(tk.Tk):
         print("selection:",file_path)
     return(file_path)    
 
-  def upload_file(self):
-    try:
-        if self.file_path == "":
-            print("no file selected")
-            return
-        print("selected: ",self.file_path)
-    except:
-        print("no file selected")
-        return   # exit if no file selected
-    self.file_name = os.path.basename(self.file_path)
-    self.local_file_time = os.path.getmtime(self.file_path)
-    self.local_file_time = datetime.datetime.utcfromtimestamp(self.local_file_time).strftime('%Y-%m-%d %H:%M:%S')
-    print("local file time:", self.local_file_time)
-    try:
-        print("FTP upload attempt, server:" , settings.FTP)
-        ftp = ftplib.FTP(settings.FTP, settings.USERNAME, settings.PASSWORD)
-# ----  check if file exists on FTP
-        try:
-            file_size = ftp.size(self.file_name)
-            file_date_string = ftp.sendcmd('MDTM ' + self.file_name)[4:]
-            file_date = datetime.datetime.strptime(file_date_string, '%Y%m%d%H%M%S')
-            message = f"The file {self.file_name} exists \n with size {file_size} bytes, \n last modified on {file_date}. \n Your file modifed on {self.local_file_time}. Rewrite file on FTP?"
-            answer = tk.messagebox.askyesno("File conflict on FTP", message)
-            if answer:
-                print("will replace file on FTP")
-            else:
-                print("Rename cancelled. The file has not been renamed.")
-                return
-        except ftplib.error_perm:
-           print(f"The file {self.file_path} does not exist on the remote server. Proceeeding to upload")
-# -----  continue with data upload
-        file_size = os.path.getsize(self.file_path)
-        # Upload the file in chunks and track the progress with tqdm
-        chunk_size = 1024 * 1024 # 1 MB
-        uploaded_size = 0
-        with tqdm(total=file_size, unit='B', unit_scale=True, desc='Uploading', miniters=1) as pbar:
-            with open(self.file_path, 'rb') as f:
-                while True:
-                    chunk = f.read(chunk_size)
-                    chunk_stream = BytesIO(chunk)
-                    if not chunk:
-                        break
-                    ftp.storbinary('STOR ' + self.file_name, chunk_stream)
-                    uploaded_size += len(chunk)
-                    pbar.update(len(chunk))
-        ftp.quit()
-        tk.messagebox.showinfo('Success','File ' + self.file_name + ' uploaded')
-    except ftplib.all_errors as e:
-        print("some issue with FTP",  e)
-        tk.messagebox.showerror("FTP error", f"Some issue with {settings.FTP} server.")
-
   def decrypt_file(self):
     self.file_path = self.file_path_entry2.get()   # get file name from second frame
     self.passphrase = self.passphrase_entry.get()   # get pass phrase from user
@@ -375,6 +509,5 @@ class App(tk.Tk):
         tk.messagebox.showinfo('Success', f'File successfully decrypted and saved as \n' + self.file_path)
  
 if __name__ == "__main__":
-
-  app = App()
-  app.mainloop()
+    app = App()
+    app.mainloop()
